524476e11acb5152ecb4c6c2b58ba4297f5b5ac0
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2025-09-10 21:22:41 +0200

    Look for sibling autoload file when installed as package

--- bootstrap.php
+++ bootstrap.php
@@ -3,7 +3,11 @@
 use PHPStan\DependencyInjection\Container;
 use PrinsFrank\PHPStanDocCodeAnalyzer\DocCodeExtractor;
 
-require __DIR__ . '/vendor/autoload.php';
+if (file_exists($installedPackagePath = dirname(__DIR__, 2) . '/autoload.php')) {
+    require $installedPackagePath;
+} else {
+    require __DIR__ . '/vendor/autoload.php';
+}
 
 /** @var Container $container */
 $container->getByType(DocCodeExtractor::class)
